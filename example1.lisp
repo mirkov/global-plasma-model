4cd8dcf51a64c4395061e1586df8853581f60180
Mirko Vukovic <mirko.vukovic@us.tel.com>
2011-11-04 18:07:32 -0400

    Fixed examples, updated README.org

--- example1.lisp
+++ example1.lisp
@@ -1,5 +1,5 @@
 ;; Mirko Vukovic
-;; Time-stamp: <2011-11-04 17:42:41EDT example1.lisp>
+;; Time-stamp: <2011-11-04 17:54:41EDT example1.lisp>
 ;; 
 ;; Copyright 2011 Mirko Vukovic
 ;; Distributed under the terms of the GNU General Public License
@@ -18,6 +18,8 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 (in-package :gpm)
+
+;; provide the three examples from L&L Section 10.2
 
 (defun example1 (&optional (stream t))
   "Walk through Example 1 of the global plasma model
@@ -54,5 +56,82 @@ L&L p. 335"
 	      (ub (ub 40 Te)))
 	  (format stream "Aeff: ~15t~5,3f~%" Aeff)
 	  (format stream "ub: ~15t~5,3e~%" ub)
-	  (let ((ne (calc-ne Pabs Aeff ub 1.0 0.0 Te)))
+	  (let ((ne (calc-ne Pabs Aeff Vs 1.0 0.0 Te)))
 	    (format stream "ne: ~15t~5,3e~%" ne))))))))
+
+(defun example2 (&optional (stream t))
+  "Walk through Example 1 of the global plasma model
+
+L&L p. 335"
+  (let ((R 0.15)
+	(L 0.3)
+	(ng 3.3e19)
+	(hl 0.31)
+	(hr 0.0)
+	(Pabs 800))
+    (format stream "Problem parameters:~%")
+    (format stream "R: ~15t~5,3f~%" R)
+    (format stream "L: ~15t~5,3f~%" L)
+    (format stream "ng: ~15t~5,3e~%" ng)
+    (format stream "pabs: ~15t~5,3e~%" Pabs)
+    (format stream "hl: ~15t~5,3f~%" hl)
+    (format stream "hr: ~15t~5,3f~%" hr)
+    (let* ((deff (deff r l :hr hr :hl hl))
+	   (Te (calc-Te (* ng deff) 1.0 0.0)))
+      (format stream "deff: ~15t~5,3f~%" deff)
+      (format stream "Te: ~15t~5,3f~%" Te)
+      (let ((Vs (Vs Te 40)))
+	(format stream "Vs: ~15t~5,3f~%" Vs)
+      (let ((Ei (Ei Vs Te))
+	    (Ee (Ee Te))
+	    (Ec (energy-loss/Ar+ Te))
+	    (Et (energy/Ar+ Vs Te)))
+	(format stream "Ei: ~15t~5,3f~%" Ei)
+	(format stream "Ee: ~15t~5,3f~%" Ee)
+	(format stream "Ec: ~15t~5,3f~%" Ec)
+	(format stream "Et: ~15t~5,3f~%" Et)
+	(let ((Aeff (Aeff R L :hr hr :hl hl))
+	      (ub (ub 40 Te)))
+	  (format stream "Aeff: ~15t~5,3f~%" Aeff)
+	  (format stream "ub: ~15t~5,3e~%" ub)
+	  (let ((ne (calc-ne Pabs Aeff Vs 1.0 0.0 Te)))
+	    (format stream "ne: ~15t~5,3e~%" ne))))))))
+
+(defun example3 (&optional (stream t))
+  "Walk through Example 1 of the global plasma model
+
+L&L p. 335"
+  (let ((R 0.15)
+	(L 0.3)
+	(ng 3.3e19)
+	(hl 0.31)
+	(hr 1.0)
+	(Pabs 800)
+	(Vs 500))
+    (format stream "Problem parameters:~%")
+    (format stream "R: ~15t~5,3f~%" R)
+    (format stream "L: ~15t~5,3f~%" L)
+    (format stream "ng: ~15t~5,3e~%" ng)
+    (format stream "pabs: ~15t~5,3e~%" Pabs)
+    (format stream "hl: ~15t~5,3f~%" hl)
+    (format stream "hr: ~15t~5,3f~%" hr)
+    (format stream "Vs: ~15t~5,3f~%" Vs)
+    (let* ((deff (deff r l :hr hr :hl hl))
+	   (Te (calc-Te (* ng deff) 1.0 0.0)))
+      (format stream "deff: ~15t~5,3f~%" deff)
+      (format stream "Te: ~15t~5,3f~%" Te)
+      (let ((Ei (Ei Vs Te))
+	    (Ee (Ee Te))
+	    (Ec (energy-loss/Ar+ Te))
+	    (Et (energy/Ar+ Vs Te)))
+	(format stream "Ei: ~15t~5,3f~%" Ei)
+	(format stream "Ee: ~15t~5,3f~%" Ee)
+	(format stream "Ec: ~15t~5,3f~%" Ec)
+	(format stream "Et: ~15t~5,3f~%" Et)
+	(let ((Aeff (Aeff R L :hr hr :hl hl))
+	      (ub (ub 40 Te)))
+	  (format stream "Aeff: ~15t~5,3f~%" Aeff)
+	  (format stream "ub: ~15t~5,3e~%" ub)
+	  (let ((ne (calc-ne Pabs Aeff Vs 1.0 0.0 Te)))
+	    (format stream "ne: ~15t~5,3e~%" ne)))))))
+
